6ebeda39a1af29dfb00b625402c940dc04d9cdc8
kradlus <markraf96@gmail.com>
2023-05-13 01:07:03 +0200

    adding python lsp

--- lua/kradlus/plugins/lsp/lspconfig.lua
+++ lua/kradlus/plugins/lsp/lspconfig.lua
@@ -1,11 +1,33 @@
-local lsp_ok, lsp = pcall(require, 'lsp-zero')
+local lsp_ok, lsp = pcall(require, "lsp-zero")
 if not lsp_ok then
-  return
+	return
 end
 
 lsp.preset({})
-lsp.on_attach(function(_, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
-end)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(_)
+		local opts = { buffer = true }
+
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "<leader>go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+
+		vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+		vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+		vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
+	end,
+})
+-- lsp.on_attach(function(_, bufnr)
+--   lsp.default_keymaps({ buffer = bufnr })
+-- end)
 
 lsp.setup()
--- lua/kradlus/plugins/lsp/null-ls.lua
+++ lua/kradlus/plugins/lsp/null-ls.lua
@@ -1,31 +1,33 @@
-local null_ls_ok, null_ls = pcall(require, 'null-ls')
+local null_ls_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_ok then
-  print("Cannot load NullLs")
-  return
+	print("Cannot load NullLs")
+	return
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-  debug = true,
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell
-  },
-  filter = function(client)
-    return client.name == "null-ls"
-  end,
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	debug = true,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.completion.spell,
+	},
+	filter = function(client)
+		return client.name == "null-ls"
+	end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
