faa185acc96e70c283ee70dd1fd150f07c3f0962
kradlus <markraf96@gmail.com>
2023-05-13 00:50:13 +0200

    updated + packer autocmd fix

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ require("kradlus.plugins.mason")
 require("kradlus.plugins.lsp.lspconfig")
 require("kradlus.plugins.lsp.mason-lspconfig")
 require("kradlus.plugins.telescope")
+require("kradlus.plugins.lsp.null-ls")
 
--- lua/kradlus/packer.lua
+++ lua/kradlus/packer.lua
@@ -3,112 +3,124 @@
 
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 -- Vim Autocmd
 vim.cmd([[
   augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source % | PackerSync
+      autocmd!
+      autocmd BufWritePost packer.lua source <afile> | PackerSync
   augroup end
 ]])
 
 local packer_bootstrap = ensure_packer()
 
-local packer_ok, packer = pcall(require, 'packer')
+local packer_ok, packer = pcall(require, "packer")
 if not packer_ok then
-  return
+	return
 end
 
-return packer.startup({function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return packer.startup({
+	function(use)
+		-- Packer can manage itself
+		use("wbthomason/packer.nvim")
 
-  -- Post-install/update hook with neovim command
-  use { 'nvim-treesitter/nvim-treesitter' }
+		-- Post-install/update hook with neovim command
+		use({ "nvim-treesitter/nvim-treesitter" })
 
-  -- Use dependency and run lua function after load
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local gitsigns_ok, gitsigns = pcall(require, 'gitsigns')
-      if not gitsigns_ok then
-        return
-      end
+		-- Use dependency and run lua function after load
+		use({
+			"lewis6991/gitsigns.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
+				if not gitsigns_ok then
+					return
+				end
 
-      -- gitsigns setup
-      gitsigns.setup()
-    end
-  }
+				-- gitsigns setup
+				gitsigns.setup()
+			end,
+		})
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = {
-      {'nvim-lua/plenary.nvim'}, {"nvim-telescope/telescope-symbols.nvim"}
-    }
-  }
+		-- Telescope
+		use({
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.1",
+			requires = {
+				{ "nvim-lua/plenary.nvim" },
+				{ "nvim-telescope/telescope-symbols.nvim" },
+			},
+		})
 
-  -- LSP
-  use({
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
+		-- LSP
+		use({
+			"VonHeikemen/lsp-zero.nvim",
+			branch = "v2.x",
+			requires = {
+				-- LSP Support
+				{ "neovim/nvim-lspconfig" }, -- Required
 
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
+				{
+					-- Optional
+					"williamboman/mason.nvim",
+					build = function()
+						pcall(vim.cmd, "MasonUpdate")
+					end,
+				},
 
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+				{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-    }
-  })
+				-- Autocompletion
+				{ "hrsh7th/nvim-cmp" }, -- Required
+				{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+				{ "L3MON4D3/LuaSnip" }, -- Required
 
-  -- Lualine
-  use ({
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-    config = function()
-      local lualine_ok, lualine = pcall(require, 'lualine')
-      if not lualine_ok then
-        return
-      end
+				-- NullLs
+				{
+					"jose-elias-alvarez/null-ls.nvim",
+					requires = { "nvim-lua/plenary.nvim" },
+				},
+			},
+		})
 
-      lualine.setup({
-        options = {
-          theme = "material"
-        }
-      })
-    end
-  })
+		-- Lualine
+		use({
+			"nvim-lualine/lualine.nvim",
+			requires = { "nvim-tree/nvim-web-devicons", opt = true },
+			config = function()
+				local lualine_ok, lualine = pcall(require, "lualine")
+				if not lualine_ok then
+					return
+				end
 
-  -- TokyoNight
-  use({ "folke/tokyonight.nvim" })
+				lualine.setup({
+					options = {
+						theme = "material",
+					},
+				})
+			end,
+		})
 
-  if packer_bootstrap then
-    packer.sync()
-  end
-end,
+		-- TokyoNight
+		use({ "folke/tokyonight.nvim" })
 
-config = {
-  display = {
-    open_fn = require('packer.util').float,
-  }
-}})
+		if packer_bootstrap then
+			packer.sync()
+		end
+	end,
+
+	config = {
+		display = {
+			open_fn = require("packer.util").float,
+		},
+	},
+})
