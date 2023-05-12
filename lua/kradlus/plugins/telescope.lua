836e348be1818ff8e8c77674a21c654e1a98e3fe
kradlus <markraf96@gmail.com>
2023-05-13 01:27:57 +0200

    adding keymap to search for symbols

--- lua/kradlus/plugins/telescope.lua
+++ lua/kradlus/plugins/telescope.lua
@@ -1,16 +1,16 @@
-local telescope_ok, telescope = pcall(require, 'telescope')
+local telescope_ok, telescope = pcall(require, "telescope")
 if not telescope_ok then
 	print("Cannot load telescope")
 	return
 end
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
-vim.keymap.set('n', '<leader>fs', builtin.symbols, {})
-vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 
 telescope.setup({})
