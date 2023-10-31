12d2d8c5b7ade4875950d0134fd2d1caffd95657
kradlus <markraf96@gmail.com>
2023-10-31 22:43:42 +0100

    updated lsp configuration

--- lua/kradlus/plugins/lsp/mason-lspconfig.lua
+++ lua/kradlus/plugins/lsp/mason-lspconfig.lua
@@ -9,17 +9,26 @@ if not lspconfig_ok then
 	return
 end
 
+local nvim_cmp_ok, nvim_cmp = pcall(require, "cmp_nvim_lsp")
+if not nvim_cmp_ok then
+	return
+end
+
 mason.setup({
 	ensure_installed = { "lua_ls", "rust_analyzer", "zls", "pyright", "pylsp", "gopls" },
 	automatic_installation = true,
 })
+
+local capabilities = nvim_cmp.default_capabilities()
 
 return mason.setup_handlers({
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
 	function(server_name) -- default handler (optional)
-		lspconfig[server_name].setup({})
+		lspconfig[server_name].setup({
+			capabilities = capabilities,
+		})
 	end,
 	["lua_ls"] = function()
 		lspconfig.lua_ls.setup({
