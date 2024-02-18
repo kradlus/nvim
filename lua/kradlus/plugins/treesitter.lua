f411a787c4ff831e64a81a53c27dde2af3b65160
kradlus <markraf96@gmail.com>
2024-02-18 09:30:11 +0100

    Adding ASM TS Parser

--- lua/kradlus/plugins/treesitter.lua
+++ lua/kradlus/plugins/treesitter.lua
@@ -3,6 +3,21 @@ if not ok then
 	return
 end
 
+---@diagnostic disable-next-line: redefined-local
+local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+if not ok then
+	print("Unable to load TreeSitter parsers")
+	return
+end
+
+parsers.get_parser_configs().asm = {
+	install_info = {
+		url = "https://github.com/rush-rs/tree-sitter-asm.git",
+		files = { "src/parser.c" },
+		branch = "main",
+	},
+}
+
 treesitter.setup({
 	ensure_installed = { "c", "python", "go", "javascript", "typescript", "lua" },
 	sync_install = false,
